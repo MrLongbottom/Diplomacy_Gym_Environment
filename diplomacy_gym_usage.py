5c2c0e3832aec9a76ff3123769c91db6be0b90be
Peter Langballe Erichsen <p.erichsen@yahoo.dk>
2021-08-18 22:09:59 +0200

    added random nn move function
    this function emulates a random output from a nn

--- diplomacy_gym_usage.py
+++ diplomacy_gym_usage.py
@@ -3,13 +3,18 @@ import random
 from diplomacy_gym_environment import DiplomacyEnvironment
 
 
-def move():
+def random_move():
     actions = {}
-
     for power_name in env.game.powers.keys():
         actions[power_name] = [random.choice(env.game.get_all_possible_orders()[loc]) for loc in
                                env.game.get_orderable_locations(power_name)]
+    return env.step(actions)
 
+
+def random_nn_move():
+    actions = {}
+    for power_name in env.game.powers.keys():
+        actions[power_name] = [random.random() for _ in env.action_list]
     return env.step(actions)
 
 
@@ -17,7 +22,7 @@ if __name__ == '__main__':
     env = DiplomacyEnvironment()
 
     while not env.game.is_game_done:
-        obs, reward, done, info = move()
+        obs, reward, done, info = random_move()
 
     print('game done.')
 
